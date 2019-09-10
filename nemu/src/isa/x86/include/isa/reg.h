044df74a80a46a6138531d8b17e938849cec4b90
tracer-ics2019 <tracer@njuics.org>
2019-09-10 16:30:01 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:30:01 up 1 day,  7:23, 12 users,  load average: 0.15, 0.11, 0.09 6bcf6613a1e33094ec48c1e4a49b406018bc2b60

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -34,7 +34,9 @@ typedef struct {
 			uint16_t _16;
 			uint8_t _8[2];
 		}gpr[8];
-		rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		struct{
+			rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
 	};
 
   /*rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;*/
