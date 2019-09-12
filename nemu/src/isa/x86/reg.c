58513c7a82fb0ce458f0a422d607719acf89283f
tracer-ics2019 <tracer@njuics.org>
2019-09-12 23:55:10 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:55:10 up 2 days,  5:01,  8 users,  load average: 0.20, 0.12, 0.10 67d817f9efac2a469f87b4b6cd8fa36ddfed734a

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -41,15 +41,15 @@ void reg_test() {
 }
 
 void isa_reg_display() {
-	printf("eax\t%#x\t%u\n",cpu.eax,cpu.eax);	
-	printf("ecx\t%#x\t%u\n",cpu.ecx,cpu.ecx);
-	printf("edx\t%#x\t%u\n",cpu.edx,cpu.edx);
-	printf("ebx\t%#x\t%u\n",cpu.ebx,cpu.ebx);
-	printf("esp\t%#x\t%u\n",cpu.esp,cpu.esp);
-	printf("ebp\t%#x\t%u\n",cpu.ebp,cpu.ebp);
-	printf("esi\t%#x\t%u\n",cpu.esi,cpu.esi);
-	printf("edi\t%#x\t%u\n",cpu.edi,cpu.edi);
-	printf("pc\t%#x\t%u\n",cpu.pc,cpu.pc);
+	printf("eax\t%#x\t\t%u\n",cpu.eax,cpu.eax);	
+	printf("ecx\t%#x\t\t%u\n",cpu.ecx,cpu.ecx);
+	printf("edx\t%#x\t\t%u\n",cpu.edx,cpu.edx);
+	printf("ebx\t%#x\t\t%u\n",cpu.ebx,cpu.ebx);
+	printf("esp\t%#x\t\t%u\n",cpu.esp,cpu.esp);
+	printf("ebp\t%#x\t\t%u\n",cpu.ebp,cpu.ebp);
+	printf("esi\t%#x\t\t%u\n",cpu.esi,cpu.esi);
+	printf("edi\t%#x\t\t%u\n",cpu.edi,cpu.edi);
+	printf("pc\t%#x\t\t%u\n",cpu.pc,cpu.pc);
 }
 
 uint32_t isa_reg_str2val(const char *s, bool *success) {
