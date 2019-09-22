2c68846007de5e67707efb93d1361efcff283625
tracer-ics2019 <tracer@njuics.org>
2019-09-22 16:49:01 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:49:01 up 1 day, 22:12,  6 users,  load average: 0.08, 0.09, 0.09 e34a62ec8845460993ff45d6425865de95ccbb44

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -7,7 +7,7 @@ uint32_t expr(char *e,bool *success);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  int is_batch_mode = init_monitor(argc, argv);
+/*  int is_batch_mode = init_monitor(argc, argv);*/
 
   FILE *fp = fopen("/home/katherine/ics2019/nemu/tools/gen-expr/input","r");
   char str[100];
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
 		else printf("%s\t%u\n",result,count_result);
 	}*/
 	/* Receive commands from user. */
-  ui_mainloop(is_batch_mode);
+  /*ui_mainloop(is_batch_mode);*/
 
   return 0;
 }
