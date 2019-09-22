748e51fa45bbc35c0f336c5fd1744dbadf06a12e
tracer-ics2019 <tracer@njuics.org>
2019-09-22 16:25:21 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:25:21 up 1 day, 21:48,  6 users,  load average: 0.10, 0.12, 0.09 abb7345b411ade5cb94eac42ae6a6585bff7809

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,12 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdint.h>
+#include<stdlib.h>
+#include "nemu.h"
+
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
+uint32_t expr(char *e,bool *success);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  int is_batch_mode = init_monitor(argc, argv);
+  //int is_batch_mode = init_monitor(argc, argv);
 
   /* Receive commands from user. */
-  ui_mainloop(is_batch_mode);
-
+  //ui_mainloop(is_batch_mode);
+    FILE * infile;
+	char input[100];
+	infile = fopen("~/ics2019/nemu/tools/gen-expr/input","r");
+	while(fgets(input,100,infile)!=NULL){
+		char *result=strtok(input," ");
+		char *expression = strtok(NULL, " ");
+		bool success=true;
+		uint32_t count_result = expr(expression,&success);
+		if(!success)printf("calculation failed!\n");
+		else printf("%s\t%u\n",result,count_result);
+	}
   return 0;
 }
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -6,9 +6,61 @@
 #include <string.h>
 
 // this should be enough
-static char buf[65536];
-static inline void gen_rand_expr() {
-  buf[0] = '\0';
+uint32_t choose(uint32_t n){
+	return rand()%n;
+}
+
+static char buf[65000];
+static inline void gen_rand_expr(int l,int r) {
+  //buf[0] = '\0';
+  if(l==r)buf[l]=choose(10)+48;
+  else if(r==l+1){
+	  switch(choose(2)){
+		  case 0:
+	          buf[l]=choose(9)+1+48;break;
+		  default:
+			  buf[l]=' ';break;
+	  }
+	  buf[r]=choose(10)+48;
+  }
+  else{
+	  int op=l+1+choose(r-l-1);
+	  switch(choose(7)){
+		  case 0:
+			  gen_rand_expr(l,op-1);
+			  buf[op]='+';
+			  gen_rand_expr(op+1,r);
+			  break;
+		  case 1:
+			  gen_rand_expr(l,op-1);
+			  buf[op]='-';
+			  gen_rand_expr(op+1,r);
+			  break;
+		  case 2:
+              gen_rand_expr(l,op-1);
+			  buf[op]='*';
+			  gen_rand_expr(op+1,r);
+			  break;
+	      case 3:
+	          gen_rand_expr(l,op-1);
+			  buf[op]='/';
+			  gen_rand_expr(op+1,r);
+			  break;
+		  case 4:
+		      buf[l]=' ';
+		      gen_rand_expr(l+1,r);
+		      break;
+		  case 5:
+		      buf[r]=' ';
+		      gen_rand_expr(l,r-1);
+		      break;	  
+		  default:
+			  buf[l]='(';
+			  buf[r]=')';
+			  gen_rand_expr(l+1,r-1);
+			  break;
+	  }
+  }
 }
 
 static char code_buf[65536];
@@ -29,7 +81,7 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
+    gen_rand_expr(0,10);
 
     sprintf(code_buf, code_format, buf);
 
