e6fe4268261623a95f1cfa9b66f1a4aecd759aa3
tracer-ics2019 <tracer@njuics.org>
2019-09-22 16:47:36 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:47:36 up 1 day, 22:11,  6 users,  load average: 0.16, 0.11, 0.09 f6a3368346b4bdc6fa5425e815e70ea88f60de27

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,7 +1,4 @@
 #include<stdio.h>
-#include<string.h>
-#include<stdint.h>
-#include<stdlib.h>
 #include "nemu.h"
 
 int init_monitor(int, char *[]);
@@ -10,11 +7,20 @@ uint32_t expr(char *e,bool *success);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  //int is_batch_mode = init_monitor(argc, argv);
+  int is_batch_mode = init_monitor(argc, argv);
 
-  /* Receive commands from user. */
-  //ui_mainloop(is_batch_mode);
-    FILE * infile;
+  FILE *fp = fopen("/home/katherine/ics2019/nemu/tools/gen-expr/input","r");
+  char str[100];
+  int result;
+  for(int i=0;i<100;i++){
+	  fscanf(fp,"%u",&result);
+	  fscanf(fp,"%s",str);
+	  bool success = true;
+	  printf("%u\n",expr(str,&success));
+	  }
+	  fclose(fp);
+
+   /* FILE * infile;
 	char input[50];
 	infile = fopen("~/ics2019/nemu/tools/gen-expr/input","r");
 	while(fgets( input,50,infile)!=NULL){
@@ -24,6 +30,9 @@ int main(int argc, char *argv[]) {
 		uint32_t count_result = expr(expression,&success);
 		if(!success)printf("calculation failed!\n");
 		else printf("%s\t%u\n",result,count_result);
-	}
+	}*/
+	/* Receive commands from user. */
+  ui_mainloop(is_batch_mode);
+
   return 0;
 }
