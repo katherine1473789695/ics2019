60709239c3670b0b068dffee27d474f186411698
tracer-ics2019 <tracer@njuics.org>
2019-10-25 23:32:10 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:32:10 up 22:01,  1 user,  load average: 0.40, 0.21, 0.12 4758c03591fd5f37d68db7afbcf2ff36e8e4cee8

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -29,7 +29,13 @@ make_EHelper(sub) {
 }
 
 make_EHelper(cmp) {
-  TODO();
+  //TODO();
+  rtl_sub(&s0,&id_dest->val,&id_src->val);
+  rtl_update_ZFSF(&s0,id_dest->width);
+  rtl_is_sub_carry(&s1,&s0,&id_dest->val);
+  rtl_set_CF(&s1);
+  rtl_is_sub_overflow(&s1,&s0,&id_dest->val,&id_src->val,id_dest->width);
+  rtl_set_OF(&s1);
 
   print_asm_template2(cmp);
 }
