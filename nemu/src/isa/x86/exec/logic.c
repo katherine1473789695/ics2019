0c48fa4a532cf1ba7b2c6a4afa2f769d014244b5
tracer-ics2019 <tracer@njuics.org>
2019-10-26 17:16:47 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:16:47 up 1 day,  2:50,  3 users,  load average: 0.24, 0.18, 0.11 3f45cffbdca4d0d0ebb352f8819e7730675542c4

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -6,6 +6,7 @@ make_EHelper(test) {
   rtl_and(&s0,&id_dest->val,&id_src->val);
   printf("%u %u",id_dest->val,id_src->val);
   rtl_update_ZFSF(&s0,id_dest->width);
+  printf("%u ",cpu.eflags.ZF);
   s0=0;
   rtl_set_CF(&s0);
   rtl_set_OF(&s0);
