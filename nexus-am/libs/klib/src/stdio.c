17ffb71f2e811deb609f553611225f2ba8121e12
tracer-ics2019 <tracer@njuics.org>
2019-10-31 00:23:31 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:23:31 up 2 days, 21:03,  1 user,  load average: 0.12, 0.16, 0.11 d709782500b89fc61c3d1fe3f85b74659801ca

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,7 +4,16 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  return 0;
+  va_list args;
+  char str[256];
+  int length;
+  va_start(args,fmt);
+  length=vsprintf(str,fmt,args);
+  va_end(args);
+  for(int i=0;str[i]!='\0';i++){
+    _putc(str[i]);
+  }
+  return length;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
