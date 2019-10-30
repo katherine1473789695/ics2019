ac06b25b029f97c75284947b1fa7f6b455e4c2fc
tracer-ics2019 <tracer@njuics.org>
2019-10-30 23:06:56 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:06:56 up 2 days, 19:47,  1 user,  load average: 0.11, 0.17, 0.11 6f3bcc27f23228218a11dd8d829f0524eebcc371

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -42,13 +42,26 @@ void pio_write_w(ioaddr_t, uint32_t);
 void pio_write_b(ioaddr_t, uint32_t);
 
 make_EHelper(in) {
-  TODO();
+  //TODO();
+  switch(id_src->width){
+    case 1:s0=pio_read_b(id_src->val);break;
+    case 2:s0=pio_read_w(id_src->val);break;
+    case 4:s0=pio_read_l(id_src->val);break;
+    default:break;
+  }
+  operand_write(id_dest,&s0);
 
   print_asm_template2(in);
 }
 
 make_EHelper(out) {
-  TODO();
+  //TODO();
+  switch(id_src->width){
+    case 1:pio_write_b(id_dest->val,id_src->width);break;
+    case 2:pio_write_w(id_dest->val,id_src->width);break;
+    case 4:pio_write_l(id_dest->val,id_src->width);break;
+    default:break;
+  }
 
   print_asm_template2(out);
 }
