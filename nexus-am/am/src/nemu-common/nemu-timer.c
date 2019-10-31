64058c84b816a9cadcb99239f7493697386fad96
tracer-ics2019 <tracer@njuics.org>
2019-10-31 19:19:53 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:19:53 up 3 days,  8:16,  1 user,  load average: 0.47, 0.34, 0.18 7720b5516eff8a2f3cf9758edafdbb3538098ff4

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -6,7 +6,7 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
-      unsigned long long time = inl(RTC_ADDR);
+      unsigned long long time = inl(0x48);
       uptime->hi = time>>32;
       uptime->lo = time & 0xffffffff;
       return sizeof(_DEV_TIMER_UPTIME_t);
