0d5c2348043ff362841d13ff8e1de11f7b4945f5
tracer-ics2019 <tracer@njuics.org>
2019-11-01 13:56:40 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  13:56:40 up  4:22,  1 user,  load average: 0.08, 0.17, 0.44 cd14692bbbe9094f269943acc91e1e926f839dc2

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -72,13 +72,15 @@ make_EHelper(dec) {
 
 make_EHelper(neg) {
   //TODO();
-  rtl_not(&s0,&id_dest->val);
-  s2=1;
-  rtl_add(&s1,&s0,&s2);
+  s0=0;
+  rtl_sub(&s1,&s0,&id_dest->val);
+  //rtl_not(&s0,&id_dest->val);
+  //s2=1;
+  //rtl_add(&s1,&s0,&s2);
   operand_write(id_dest,&s1);
 
   rtl_update_ZFSF(&s1,id_dest->width);
-  rtl_is_add_overflow(&s1,&s1,&s0,&s2,id_dest->width);
+  rtl_is_sub_overflow(&s1,&s1,&s0,&id_dest->val,id_dest->width);
   rtl_set_OF(&s1);
   if(id_dest->val!=0)s1=1;
   else s1=0;
