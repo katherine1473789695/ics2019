cd019d414c7a7d337b88cfdb6f2ac459cd7155e1
tracer-ics2019 <tracer@njuics.org>
2019-11-02 14:06:41 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:06:41 up 18:30,  1 user,  load average: 0.17, 0.13, 0.10 3dec434c16bc4a97877db149c812ecac28326ef

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -55,6 +55,11 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
         }
         break;
       }
+      case 'c':{
+        int num=va_arg(ap,int);
+        *str++=(char)num;
+        break;
+      }
       default:break;
     }
   }
