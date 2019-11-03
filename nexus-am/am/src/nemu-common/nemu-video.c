10ffb75a1960eb5dec84f51a4993b524e1ea26a7
tracer-ics2019 <tracer@njuics.org>
2019-11-03 14:04:41 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:04:41 up 1 day,  8:59,  1 user,  load average: 0.22, 0.15, 0.16 eee713ece8fe3a2e7ecf92b2ee89db097e55533c

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -2,6 +2,9 @@
 #include <amdev.h>
 #include <nemu.h>
 
+#define W 400
+#define H 300
+
 void draw_sync();
 int screen_width();
 int screen_height();
@@ -11,8 +14,8 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
       //uint32_t data=inl(SCREEN_ADDR);
-      info->width = 400;//data>>16;
-      info->height = 300;//data & 0xffff;
+      info->width = W;//data>>16;
+      info->height = H;//data & 0xffff;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -23,16 +26,16 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-      int width = screen_width();
+      //int width = screen_width();
       int x=ctl->x,y=ctl->y,h=ctl->h,w=ctl->w;
       uint32_t *pixels = ctl->pixels;
       uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
       for(int i=0;i<h;i++){
         for(int j=0;j<w;j++){
-          fb[(y+i)*width+x]=pixels[i];
+          fb[(y+i)*W+x]=pixels[i];
         }
       }
-
+    
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
