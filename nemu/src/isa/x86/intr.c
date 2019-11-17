07f70920534ad6e931ccf40d7412cb3a7bb0391b
tracer-ics2019 <tracer@njuics.org>
2019-11-17 22:03:10 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:03:10 up 1 day, 13:59,  1 user,  load average: 0.76, 0.35, 0.23 1908132f3d21866455c712868035e1de01b3e7b

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -1,9 +1,20 @@
 #include "rtl/rtl.h"
+#include "cpu/exec.h"
+#include "isa/mmu.h"
 
 void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+   rtl_push(&cpu.eflags_value);
+   rtl_push(&cpu.cs);
+   rtl_push(&ret_addr);
+   vaddr_t idt_addr = cpu.idtr.base + sizeof(GateDesc)*NO;
+   vaddr_t low_addr = vaddr_read(idt_addr,2) & 0xffff;
+   vaddr_t high_addr = vaddr_read(idt_addr+4,4) & 0xffff0000;
+   decinfo.jmp_pc = high_addr | low_addr;
+   decinfo.is_jmp = 1;
+
 }
 
 bool isa_query_intr(void) {
