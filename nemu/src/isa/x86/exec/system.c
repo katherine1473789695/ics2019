fc9651c16e537d9ecaa6df17de0d525cb1c6a2e5
tracer-ics2019 <tracer@njuics.org>
2019-11-19 23:31:03 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:31:03 up 2 days,  4:50,  1 user,  load average: 0.15, 0.14, 0.13 1b550bcc9bfb7f99d8b55a459eef38d5d3224d0b

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -40,8 +40,10 @@ make_EHelper(int) {
 make_EHelper(iret) {
   //TODO();
   rtl_pop(&s0);
-  decinfo.jmp_pc = s0;
-  decinfo.is_jmp = 1;
+  //decinfo.jmp_pc = s0;
+  //decinfo.is_jmp = 1;
+  decinfo.is_jmp=0;
+  decinfo.seq_pc = s0;
   rtl_pop(&s0);
   cpu.cs = s0;
   rtl_pop(&s0);
