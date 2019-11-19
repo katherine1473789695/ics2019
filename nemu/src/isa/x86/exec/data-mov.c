e6fb099c16ddd3a9b4d3f82b7362616b56416f43
tracer-ics2019 <tracer@njuics.org>
2019-11-19 20:05:58 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:05:58 up 2 days,  2:19,  1 user,  load average: 0.29, 0.16, 0.18 6d53c491c2b90be8271f673da7890f07bac2bcf

--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -21,7 +21,17 @@ make_EHelper(pop) {
 }
 
 make_EHelper(pusha) {
-  TODO();
+  //TODO();
+  s0=cpu.esp;
+  rtl_push(&cpu.eax);
+  rtl_push(&cpu.ecx);
+  rtl_push(&cpu.edx);
+  rtl_push(&cpu.ebx);
+  rtl_push(&s0);
+  rtl_push(&cpu.ebp);
+  rtl_push(&cpu.esi);
+  rtl_push(&cpu.edi);
+
 
   print_asm("pusha");
 }
