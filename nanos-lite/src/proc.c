9078012a559982ca18cdb68b6a8a9168ea393bb9
tracer-ics2019 <tracer@njuics.org>
2019-11-20 18:51:34 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  18:51:34 up 2 days, 10:24,  1 user,  load average: 0.38, 0.22, 0.13 50dedb94b0669451c97bcccca92e5bfd70699c9

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -5,6 +5,7 @@
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
+void naive_uload(PCB *pcb, const char *filename);
 
 void switch_boot_pcb() {
   current = &pcb_boot;
@@ -25,6 +26,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
+  naive_uload(NULL, NULL);
 
 }
 
