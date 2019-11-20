185336757657137293f88f7b9e18c3233125b710
tracer-ics2019 <tracer@njuics.org>
2019-11-20 19:58:34 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:58:34 up 2 days, 11:31,  1 user,  load average: 0.17, 0.16, 0.10 8832462ac8d06de1b38d8cc6427689f2c962cd68

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,15 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
+#define ENTRY ((void* )0x3000000)
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  //TODO();
+  //void* buf;
+  ramdisk_read(ENTRY,0,get_ramdisk_size());
+  return (uintptr_t)ENTRY;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
