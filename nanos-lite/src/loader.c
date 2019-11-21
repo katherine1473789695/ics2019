75a25d01b926dd854ee7bd3a9e3d0c5d7998b8ec
tracer-ics2019 <tracer@njuics.org>
2019-11-21 14:49:01 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:49:01 up 2 days, 18:23,  1 user,  load average: 0.19, 0.52, 0.52 7f53d3cf6caae4c1ede0331d68945fd648d8e964

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,8 +16,17 @@ size_t get_ramdisk_size();
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   uintptr_t program_entry;
+  uint32_t seg_off;
+  uint16_t shentsize;
+  uint16_t shnum;
   ramdisk_read(&program_entry,24,4);
-  printf("%x",program_entry);
+  ramdisk_read(&seg_off,32,4);
+  ramdisk_read(&shentsize,46,2);
+  ramdisk_read(&shnum,48,2);
+  printf("%x\n",program_entry);
+  printf("%x\n",seg_off);
+  printf("%x\n",shentsize);
+  printf("%x\n",shnum);
   return program_entry;
 }
 
