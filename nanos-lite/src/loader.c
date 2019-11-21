367e14d1296bcede237b83d89fb0d9fee619910f
tracer-ics2019 <tracer@njuics.org>
2019-11-21 16:20:21 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:20:21 up 2 days, 19:47,  1 user,  load average: 0.23, 0.21, 0.19 f43055bacf3d96f5fde63902496bdeac98d9a97d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,27 +16,27 @@ size_t get_ramdisk_size();
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   Elf_Ehdr elfheader;
-  Elf_Phdr programheader;
+  //Elf_Phdr programheader;
 
   ramdisk_read(&elfheader,0,sizeof(Elf_Ehdr));
-  uint16_t num = elfheader.e_phnum;
-  uint32_t offset= elfheader.e_phoff;
-  uint16_t size=elfheader.e_phentsize;
-  uintptr_t  addr= elfheader.e_entry;
-  printf("%x\n",offset);
-  printf("%x\n",size);
-  printf("%x\n",num);
+  //uint16_t num = elfheader.e_phnum;
+  //uint32_t offset= elfheader.e_phoff;
+  //uint16_t size=elfheader.e_phentsize;
+  //uintptr_t  addr= elfheader.e_entry;
+  //printf("%x\n",offset);
+  //printf("%x\n",size);
+  //printf("%x\n",num);
   //printf("%x\n",get_ramdisk_size());
   //while(num--){
-    ramdisk_read(&programheader,offset,size);
-    if(programheader.p_type==PT_LOAD){
-      uint32_t data;
-      ramdisk_read(&data,programheader.p_offset,programheader.p_filesz);
-      offset+=size;
-    }
+    //ramdisk_read(&programheader,offset,size);
+    //if(programheader.p_type==PT_LOAD){
+      //uint32_t data;
+      //ramdisk_read(&data,programheader.p_offset,programheader.p_filesz);
+      //offset+=size;
+    //}
   //}
   
-  return addr;
+  return elfheader.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
