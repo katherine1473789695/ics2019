33af805d77e6283036391541d5e40a54a19b17ed
tracer-ics2019 <tracer@njuics.org>
2019-11-21 16:41:11 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:41:11 up 2 days, 20:08,  1 user,  load average: 0.19, 0.18, 0.18 b0a0a205eb63ff408d7d07dc760ea47901e89ec

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include "proc.h"
 #include <elf.h>
+#include "memory.h"
 
 #ifdef __ISA_AM_NATIVE__
 # define Elf_Ehdr Elf64_Ehdr
@@ -21,6 +22,11 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   ramdisk_read(&elfheader,0,sizeof(Elf_Ehdr));
   for(uint16_t i=0;i<elfheader.e_phnum;i++){
     ramdisk_read(&programheader,elfheader.e_phoff+i*elfheader.e_phentsize,sizeof(Elf_Phdr));
+    if(programheader.p_type == PT_LOAD){
+      //uint8_t buf[programheader.p_filesz];
+      ramdisk_read(&programheader.p_vaddr,programheader.p_offset,programheader.p_filesz);
+      //vaddr_write(programheader.p_vaddr,&buf,programheader.p_filesz);
+    }
     printf("%x\n",programheader.p_type);
     printf("%x\n",programheader.p_vaddr);
   }
