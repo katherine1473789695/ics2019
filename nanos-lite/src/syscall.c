f4635efb8c81e0fc3aa2875548ed14b0716211be
tracer-ics2019 <tracer@njuics.org>
2019-11-22 16:40:47 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:40:47 up 3 days,  8:10,  1 user,  load average: 0.22, 0.15, 0.12 cfa5787b8f2b4c6ab224e3717ecd2345e409fa66

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,18 +4,21 @@
 
 int sys_yield();
 void sys_exit(uintptr_t arg);
+size_t sys_write(int fd,const void *buf,size_t count);
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   uintptr_t result=-1;
   //printf("%x\n",c->GPR1);
   printf("%d\n",a[0]);
   switch (a[0]) {
     case SYS_exit:sys_exit(a[1]);break;
     case SYS_yield: result=sys_yield();break;
-    case SYS_write:
+    case SYS_write: result = sys_write(a[1],(void*)a[2],a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   c->GPRx=result;
@@ -30,4 +33,13 @@ int sys_yield(){
 
 void sys_exit(uintptr_t arg){
   _halt(arg);
+}
+
+size_t sys_write(int fd,const void *buf,size_t count){
+  if(fd==1||fd==2){
+    for(int i=0;i<count;i++){
+      _putc(((char *)buf)[i]);
+    }
+  }
+  return count;
 }
