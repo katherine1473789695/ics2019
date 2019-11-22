526727068a555c30b0def11686d56aa5ee4e4c0a
tracer-ics2019 <tracer@njuics.org>
2019-11-22 16:25:38 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:25:38 up 3 days,  7:59,  1 user,  load average: 0.16, 0.18, 0.12 4935d01c59d8b8c4ad2960a7bad42dd7bc54d011

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,10 @@
 #include "common.h"
 #include "syscall.h"
 
+
+int sys_yield();
+void sys_exit(uintptr_t arg);
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -9,12 +13,21 @@ _Context* do_syscall(_Context *c) {
   //printf("%x\n",c->GPR1);
   printf("%d\n",a[0]);
   switch (a[0]) {
-    case SYS_exit:_halt(a[1]);break;
-    case SYS_yield: {_yield();result=0;break;}
+    case SYS_exit:sys_exit(a[1]);break;
+    case SYS_yield: result=sys_yield();break;
     case SYS_write:
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   c->GPRx=result;
 
   return NULL;
+}
+
+int sys_yield(){
+  _yield();
+  return 0;
+}
+
+void sys_exit(uintptr_t arg){
+  _halt(arg);
 }
