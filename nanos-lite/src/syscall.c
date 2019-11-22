6ffc6e713af1af885ffcd06137d8ea8933f7a2fb
tracer-ics2019 <tracer@njuics.org>
2019-11-22 08:58:49 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  08:58:49 up 3 days,  2:45,  1 user,  load average: 0.37, 0.29, 0.22 ab61390496982007329e23ed3c7fe81e6ba572ef

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,6 +5,7 @@ _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   uintptr_t result=-1;
+  printf("%x\n",c->eax);
 
   switch (a[0]) {
     case SYS_yield: _yield();result=0;break;
