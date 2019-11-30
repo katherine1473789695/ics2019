b099836566904187a701f7bdb55842728d9859a9
tracer-ics2019 <tracer@njuics.org>
2019-11-30 17:44:59 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:44:59 up 5 days, 17:23,  1 user,  load average: 0.40, 0.26, 0.20 f883e598054528914baaf1aa5ba5889d452a0e

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -71,7 +71,7 @@ size_t fs_read(int fd,void *buf,size_t len){
   if(f->read!=NULL){
     read = f->read(buf,f->open_offset,read);
   }else{
-    read = (f->open_offset+len>f->size) ? (f->size-f->open_offset):len;
+    //read = (f->open_offset+len>f->size) ? (f->size-f->open_offset):len;
     ramdisk_read(buf,f->disk_offset+f->open_offset,read);
     f->open_offset+=read;
   }
@@ -105,10 +105,11 @@ size_t fs_write(int fd,const void *buf,size_t len){
   assert(0<=fd && fd<NR_FILES);
   size_t write=-1;
   Finfo *f=&file_table[fd];
+  write = (f->open_offset+len > f->size) ? (f->size-f->open_offset) : len;
   if(f->write!=NULL){
-    write = f->write(buf,f->open_offset,len);
+    write = f->write(buf,f->open_offset,write);
   }else{
-    write = (f->open_offset+len > f->size) ? (f->size-f->open_offset) : len;
+    //write = (f->open_offset+len > f->size) ? (f->size-f->open_offset) : len;
     ramdisk_write(buf,f->disk_offset+f->open_offset,write);
     f->open_offset+=write;
   }
