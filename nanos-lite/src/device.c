045e608851d1db06bdb882863585a0e7ccc173b4
tracer-ics2019 <tracer@njuics.org>
2019-11-30 23:29:27 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:29:27 up 5 min,  1 user,  load average: 0.81, 0.77, 0.38 b7fb37f48c92f19937af85c7f83f5759dfd9bf18

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -21,6 +21,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   Log("here");
   uint32_t time = uptime();
   Log("time: %d",time);
+  sprintf(buf,"t %d\n",uptime());
   int key = read_key();
   bool down =false;
   if(key & 0x8000){
