a171491515740a10e7bd6e127e6efa94da495db7
tracer-ics2019 <tracer@njuics.org>
2019-11-30 12:48:25 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  12:48:25 up 5 days, 13:40,  1 user,  load average: 0.42, 0.22, 0.13 8a98e78b651bda0b6c4a3a9cead121da1ce4f45c

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -2,6 +2,9 @@
 #define __FS_H__
 
 #include "common.h"
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t serial_write(const void *buf, size_t offset, size_t len);
 
 #ifndef SEEK_SET
 enum {SEEK_SET, SEEK_CUR, SEEK_END};
