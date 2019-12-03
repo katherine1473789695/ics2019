60a4a915c2453daf6d048d3ec05bd04d3475e4cf
tracer-ics2019 <tracer@njuics.org>
2019-12-03 21:07:33 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:07:33 up 1 day,  4:53,  1 user,  load average: 0.33, 0.50, 0.71 c1d1db811917d2914fc8e50d49cd07b1af539a0

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -60,7 +60,7 @@ static OpcodeEntry opcode_table [512] = {
   /* 0x14 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x18 */	EMPTY, IDEX(G2E,sbb), EMPTY, IDEX(E2G,sbb),
   /* 0x1c */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x20 */	EMPTY, IDEX(G2E,and), IDEXW(E2G,and,1), IDEX(E2G,and),
+  /* 0x20 */	IDEXW(G2E,and,1), IDEX(G2E,and), IDEXW(E2G,and,1), IDEX(E2G,and),
   /* 0x24 */	IDEXW(I2a,and,1), IDEX(I2a,and), EMPTY, EMPTY,
   /* 0x28 */	EMPTY, IDEX(G2E,sub), EMPTY, IDEX(E2G,sub),
   /* 0x2c */	EMPTY, IDEX(I2a,sub), EMPTY, EMPTY,
