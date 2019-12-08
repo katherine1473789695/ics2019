36d357baeeee1a6a06a787899aa52141df1f61ae
tracer-ics2019 <tracer@njuics.org>
2019-12-08 20:37:47 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:37:47 up 3 days,  2:53,  1 user,  load average: 0.28, 0.15, 0.10 b5b6eba23d7a3bff740d9eff6749687b52a70c9

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,6 +2,7 @@
 #include <amdev.h>
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+  _yield();
   for(int i=0;i<len;i++){
     _putc(((char *)buf)[i]);
   }
@@ -17,6 +18,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+  _yield();
   //sprintf(buf,"t %d\n",uptime());
   //Log("here");
   //uint32_t time = uptime();
@@ -54,6 +56,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   //Log("fb_write\n");
+  _yield();
   
   int x,y;
   offset = offset>>2;
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -2,6 +2,7 @@
 
 #define MAX_NR_PROC 4
 void context_kload(PCB *pcb, void *entry);
+void context_uload(PCB *pcb, const char *filename);
 
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
@@ -23,6 +24,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   context_kload(&pcb[0],(void*)hello_fun);
+  context_uload(&pcb[1], "/bin/init");
 
   switch_boot_pcb();
 
@@ -35,6 +37,7 @@ void init_proc() {
 
 _Context* schedule(_Context *prev) {
   current->cp = prev;
-  current = &pcb[0];
+  //current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current->cp;
 }
