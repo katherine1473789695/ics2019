bba67d8e23e442557eea52d84d777284e7652b0b
tracer-ics2019 <tracer@njuics.org>
2019-12-13 20:14:45 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:14:45 up 4 days, 17:52,  1 user,  load average: 0.43, 0.30, 0.22 783cdb841f88b5ecaebb549999fe4b8bb979dc5f

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -7,7 +7,7 @@ static paddr_t page_translate(vaddr_t addr){
   PTE pte;
   cr3.val = cpu.cr3;
   printf("%x\n",cr3.val);
-  pde.val = paddr_read((cr3.page_directory_base<<12) + PDE_INDEX(addr) * sizeof(PDE), sizeof(PDE));
+  pde.val = paddr_read(cr3.page_directory_base + PDE_INDEX(addr) * sizeof(PDE), sizeof(PDE));
   //printf("%x\n",pde.val);
   assert(pde.present == 1);
   pte.val = paddr_read(pde.page_frame + PTE_INDEX(addr) * sizeof(PTE), sizeof(PTE));
