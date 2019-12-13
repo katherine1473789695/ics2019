14c4c7195eb9a2b66e03f16e4d6a15158ae10a88
tracer-ics2019 <tracer@njuics.org>
2019-12-13 16:43:20 +0800

    >  compile 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:43:20 up 4 days, 14:57,  1 user,  load average: 0.33, 0.23, 0.18 bc2dbacc48d9ce6295f1aeeb6b24480120671d55

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -56,3 +56,4 @@ make_EHelper(iret);
 make_EHelper(movsb);
 make_EHelper(movsw);
 make_EHelper(mov_r2cr);
+make_EHelper(mov_cr2r);
--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -24,7 +24,12 @@ make_EHelper(mov_r2cr) {
 }
 
 make_EHelper(mov_cr2r) {
-  TODO();
+  //TODO();
+  switch(id_src->reg){
+    case 0:operand_write(id_dest,&cpu.cr0);break;
+    case 3:operand_write(id_dest,&cpu.cr3);break;
+    default:panic("invalid cr number");
+  }
 
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
