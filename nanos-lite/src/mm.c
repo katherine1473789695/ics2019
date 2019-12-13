a2fd85a31fb488a4bac92178e5416731d283bd57
tracer-ics2019 <tracer@njuics.org>
2019-12-13 19:57:42 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:57:42 up 4 days, 17:35,  1 user,  load average: 0.32, 0.18, 0.17 b7328f259df37208f03f3b6472979da9c3af01c5

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -20,6 +20,7 @@ int mm_brk(uintptr_t brk, intptr_t increment) {
 
 void init_mm() {
   pf = (void *)PGROUNDUP((uintptr_t)_heap.start);
+  panic("hi");
   Log("free physical pages starting from %p", pf);
 
   _vme_init(new_page, free_page);
