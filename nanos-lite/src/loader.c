3f975327502712544d45bdd774ff9ed21e7e4a71
tracer-ics2019 <tracer@njuics.org>
2019-12-14 01:06:04 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:06:04 up 4 days, 21:10,  1 user,  load average: 0.20, 0.16, 0.12 9ca600bac81e43369060da9cab247f2de776fe5d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -98,6 +98,7 @@ void context_kload(PCB *pcb, void *entry) {
 }
 
 void context_uload(PCB *pcb, const char *filename) {
+  _protect(&pcb->as);
   uintptr_t entry = loader(pcb, filename);
 
   _Area stack;
