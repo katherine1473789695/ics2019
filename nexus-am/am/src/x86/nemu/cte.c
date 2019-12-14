0e76a9b6268c45abc8fe499487de149411e391fc
tracer-ics2019 <tracer@njuics.org>
2019-12-14 19:20:37 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:20:37 up 5 days,  1:03,  1 user,  load average: 0.15, 0.21, 0.19 3eda93b8d39b7430226ea205cf5972103aa90162

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -11,6 +11,7 @@ void __am_get_cur_as(_Context *c);
 void __am_switch(_Context *c);
 
 _Context* __am_irq_handle(_Context *c) {
+  _putc('h');
   _Context *next = c;
   __am_get_cur_as(c);
   //_putc(c->edi);
