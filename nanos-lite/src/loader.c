5d1c1e55d67ada546990a8ada50846e8834b11cb
tracer-ics2019 <tracer@njuics.org>
2019-12-15 13:55:50 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  13:55:50 up 5 days, 10:31,  1 user,  load average: 0.36, 0.22, 0.19 5afc0b488e97879022af69fbd04c025699e4dfdf

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -49,6 +49,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
           //printf("%x\n",paddr);
           _map(&pcb->as,vaddr,paddr,0);
           fs_read(fd,paddr,read_bytes);
+          pcb->max_brk = PGROUNDUP((uintptr_t)vaddr+read_bytes);
           //memset((void*)paddr+programheader.p_filesz,0,(programheader.p_memsz-programheader.p_filesz));
         }
         //void *pa = new_page(1);
