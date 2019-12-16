e629c05baeafcf2df85b253bad084b9c885b2fdb
tracer-ics2019 <tracer@njuics.org>
2019-12-16 13:53:02 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  13:53:02 up 5 days, 20:22,  5 users,  load average: 1.05, 1.05, 0.66 d2281dfec9b58944b22d287db50a718d5c5f8e2

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   //context_kload(&pcb[0],(void*)hello_fun);
-  context_uload(&pcb[1], "/bin/bmptest");
+  context_uload(&pcb[1], "/bin/pal");
 
   switch_boot_pcb();
   //Log("the end of the proc");
