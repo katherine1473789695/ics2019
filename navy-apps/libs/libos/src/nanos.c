598862ae7335b958bc2739b33c44d2b662735ab3
tracer-ics2019 <tracer@njuics.org>
2019-12-16 13:15:21 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  13:15:21 up 5 days, 19:44,  5 users,  load average: 0.30, 0.16, 0.16 ba0371414e2cb2be3ed0000533241598bcc88a24

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -63,10 +63,11 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 extern char _end;
-static uintptr_t program_break = (uintptr_t)&_end;
+
 void *_sbrk(intptr_t increment) {
-  uintptr_t old = program_break;
-  if(_syscall_(SYS_brk,increment,0,0)==0){
+  static void* program_break = (uintptr_t)&_end;
+  void* old = program_break;
+  if(_syscall_(SYS_brk,(uintptr_t)program_break+increment,0,0)==0){
     program_break+=increment;
     return(void*)old;
   }
