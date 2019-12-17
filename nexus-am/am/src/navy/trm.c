9350c67692bae698ff531e2759bd89034ce36c2b
tracer-ics2019 <tracer@njuics.org>
2019-12-17 15:37:29 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  15:37:29 up 6 days,  9:16,  5 users,  load average: 0.52, 0.23, 0.18 17f5bf422c252ec2922fa22fdc9676c9de6f98e2

--- nexus-am/am/src/navy/trm.c
+++ nexus-am/am/src/navy/trm.c
@@ -1,27 +1,13 @@
 #include <am.h>
-#include <nemu.h>
 
-extern char _heap_start;
-extern char _heap_end;
-int main(const char *args);
-
-_Area _heap = {
-  .start = &_heap_start,
-  .end = &_heap_end,
-};
+_Area _heap = {};
 
 void _trm_init() {
-  extern const char _start;
-  const char *mainargs = &_start - 0x100000;
-  int ret = main(mainargs);
-  _halt(ret);
+
 }
 
 void _putc(char ch) {
-  outb(SERIAL_PORT, ch);
 }
 
 void _halt(int code) {
-  nemu_trap(code);
-  while (1);
 }
