bc0414d9fcf5fba76ad49ee95d7412eef1405cb9
tracer-ics2019 <tracer@njuics.org>
2019-12-17 09:29:14 +0800

    >  run 181840326 katherine Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  09:29:14 up 6 days,  4:47,  5 users,  load average: 0.56, 0.24, 0.13 934f634bdfb7b688bba7cf812110a051cb8469d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -41,7 +41,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
         void *vaddr, *paddr;
         vaddr = (void*)programheader.p_vaddr;
         //printf("%x\n",vaddr);
-        for(size_t i=0,sz = programheader.p_filesz;i<sz;i+=PGSIZE){
+        for(size_t i=0,sz = programheader.p_memsz;i<sz;i+=PGSIZE){
           size_t read_bytes = ((sz-i)>=PGSIZE) ? PGSIZE : (sz-i);
           //printf("%x\n",read_bytes);
           paddr = new_page(1);
